89f4bfb9baa3c04b3b3ac8fa61dde0a6431f8206
pawanroy <proy@teckborn.com>
2022-07-19 20:27:20 +0530

    SplashActivity.kt

--- app/src/main/java/com/simpleapps/pdfcomposerapplication/activities/SplashActivity.kt
+++ app/src/main/java/com/simpleapps/pdfcomposerapplication/activities/SplashActivity.kt
@@ -1,17 +1,22 @@
 package com.simpleapps.pdfcomposerapplication.activities
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.simpleapps.pdfcomposerapplication.activities.ui.theme.PdfComposerApplicationTheme
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : ComponentActivity() {
@@ -24,22 +29,26 @@ class SplashActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
+
+        Executors.newSingleThreadScheduledExecutor().schedule({
+            startActivity(Intent(this, IntroActivity::class.java))
+        }, 1,TimeUnit.SECONDS)
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting() {
+    Icon(Icons.Filled.Warning, contentDescription = null)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     PdfComposerApplicationTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
